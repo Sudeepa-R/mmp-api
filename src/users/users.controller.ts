f337aa5131ca71d92ec42d70e897ea7c8cddc042
Sudeepa-R <sudeepar084@gmail.com>
2024-12-18 20:20:21 +0530

    swagger added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,20 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDetails } from './users.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
-    constructor(
-        private readonly userServices:UsersService
-    ){}
+  constructor(private readonly userServices: UsersService) {}
 
-    @Get(':id')
-    async getUserById(@Param('id') id:number):Promise<UserDetails>{
-        return await this.userServices.getDataById(id)
-    }
+  @Get(':id')
+  async getUserById(@Param('id') id: number): Promise<UserDetails> {
+    return await this.userServices.getDataById(id);
+  }
 
-    @Post()
-    async saveUsers(@Body()userData:UserDetails):Promise<UserDetails>{
-        return this.userServices.saveData(userData)
-    }
+  @Post()
+  async saveUsers(@Body() userData: UserDetails): Promise<UserDetails> {
+    return this.userServices.saveData(userData);
+  }
 }
