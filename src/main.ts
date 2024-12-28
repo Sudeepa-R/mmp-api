2c0ecc6e6640c39f144991602528673a1e2cd065
Sudeepa-R <sudeepar084@gmail.com>
2024-12-28 17:14:33 +0530

    vercel issue

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigModule } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin:[ 'http://localhost:5173','https://prod-mmp-api.vercel.app/'],
@@ -30,6 +34,8 @@ async function bootstrap() {
   SwaggerModule.setup('/', app, document, {
     swaggerOptions: { docException: 'none', tagsSorter: 'alpha' },
   });
+  await app.init();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+export default server;
